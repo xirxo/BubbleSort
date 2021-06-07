13ef046c9490d63e5466fe2eec63d88de5b40a35
xirxo <85446025+xirxo@users.noreply.github.com>
2021-06-07 11:02:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ var container = document.getElementById("array");
 
 function generatearray() {
     for (var i = 0; i < 20; i++) {
-  
         var value = Math.ceil(Math.random() * 100);
-  
         var array_ele = document.createElement("div");
   
         array_ele.classList.add("block");
-  
         array_ele.style.height = `${value * 3}px`;
         array_ele.style.transform = `translate(${i * 30}px)`;
 
@@ -23,17 +20,15 @@ function generatearray() {
 
 function swap(el1, el2) {
     return new Promise((resolve) => {
-  
         var temp = el1.style.transform;
         el1.style.transform = el2.style.transform;
         el2.style.transform = temp;
   
         window.requestAnimationFrame(function() {
-  
             setTimeout(() => {
                 container.insertBefore(el2, el1);
                 resolve();
-            }, 250);
+            }, 200);
         });
     });
 }
@@ -43,35 +38,31 @@ async function BubbleSort(delay = 100) {
 
     for (var i = 0; i < blocks.length; i += 1) {
         for (var j = 0; j < blocks.length - i - 1; j += 1) {
-  
             blocks[j].style.backgroundColor = "#FF4949";
             blocks[j + 1].style.backgroundColor = "#FF4949";
-  
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
                 }, delay)
             );
-  
+
             var value1 = Number(blocks[j].childNodes[0].innerHTML);
-            var value2 = Number(blocks[j + 1]
-                        .childNodes[0].innerHTML);
+            var value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
   
-            // To compare value of two blocks
             if (value1 > value2) {
                 await swap(blocks[j], blocks[j + 1]);
                 blocks = document.querySelectorAll(".block");
             }
   
-            // Changing the color to the previous one
             blocks[j].style.backgroundColor = "#6b5b95";
             blocks[j + 1].style.backgroundColor = "#6b5b95";
         }
   
-        blocks[blocks.length - i - 1]
-                .style.backgroundColor = "#13CE66";
+        blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
     }
 }
-  
+
 generatearray();
-BubbleSort();
+setTimeout(() => {
+    BubbleSort();
+}, 2000);
